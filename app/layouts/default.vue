73f3bd67601581dd4b86a26457c6274ba99f9dd6
roiLeo <medina.leo42@gmail.com>
2025-03-06 16:30:32 +0100

    Initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex min-h-screen flex-col">
+    <DefaultNavbar />
+
+    <UContainer class="w-full">
+      <NuxtPage />
+    </UContainer>
+
+    <DefaultFooter />
+  </div>
+</template>
